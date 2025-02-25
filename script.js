2a784adaadf4c1513060aaadf004a80b535b6425
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-25 17:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click",function(){
+	const ab = document.getElementById("status");
+	ab.outerHTML = "<h1 id='status'>Entered Metaverse</h1>"
+})
