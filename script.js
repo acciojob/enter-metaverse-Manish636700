e3acf030e03669a3449d8db550bc6a05678e1ee6
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-25 18:26:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 //your JS code here. If required.
 document.getElementById("enterBtn").addEventListener("click",function(){
 	let ab = document.getElementById("status");
-	let newa = document.createElement("h1");
-	newa.id="status";
-	newa.textContent = "Entered Metaverse";
-	ab.replaceWith(newa);
+	ab.innerHTML= `<h1>Entered Metaverse</h1>`
 });
